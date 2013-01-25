6ab10b06557a5e3f3f5ee27f4cc7b02e8fb15bba
James Kong <fishkingsin@gmail.com>
2013-01-25 18:44:35 +0800

    bug fix

--- src/ofxDraggable.h
+++ src/ofxDraggable.h
@@ -1,22 +1,39 @@
-class ofxDraggable : public ofxMSAInteractiveObject {  
+#include "ofxMSAInteractiveObject.h"
+class ofxDraggable : public ofxMSAInteractiveObject {
 public:  
     ofBaseDraws *content; // this will point to your image, video, grabber etc.  
       
     ofxDraggable() {  
-        content = NULL;  
-    }  
+        content = NULL;
+		saveX = 0;
+		saveY = 0;
+		onPressed = false;
+    }
+	void setPos(float x , float y)
+	{
+		ofxMSAInteractiveObject::setPos(x, y);
+		saveX = x;
+		saveY = y;
+	}
       
     void onPress(int mx, int my, int button) {  
         // save the offset of where the mouse was clicked...  
         // ...relative to the position of the object  
         saveX = mx - this->x;  
-        saveY = my - this->y;  
+        saveY = my - this->y;
+		onPressed = true;
     }  
       
-    void onDragOver(int mx, int my, int button) {  
+    void onDragOver(int mx, int my, int button) {
+		if(!onPressed) return;
         this->x = mx - saveX;    // update x position  
         this->y = my - saveY;    // update mouse y position  
-    }  
+    }
+	void onRelease(int x, int y, int button)
+	{
+		onPressed = false;
+	}
+	
       
     void draw() {  
         if(content) {  
@@ -37,6 +54,7 @@ public:
         }  
     }  
       
-protected:  
+protected:
+	bool onPressed;
     int saveX, saveY;  
 };  
